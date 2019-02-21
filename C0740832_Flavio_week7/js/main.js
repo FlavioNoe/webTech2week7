c2857b20c1d8c154bf2aec18d47f15b4fb3e18c1
FlavioNoe <flavio.noe.santos@gmail.com>
2019-02-21 12:04:05 -0500

    12h03min - final Commit.

--- C0740832_Flavio_week7/js/main.js
+++ C0740832_Flavio_week7/js/main.js
@@ -1,8 +1,8 @@
 ﻿var bird = {
     genus: "corvus",
-    species: "corvus",
+    species: "corvax",
     commonName: "raven",
-    callType: "aquawky",
+    callType: "squawky",
     quote: "Nevermore",
     maxOffspring: 5,
     noisy: true,
@@ -19,3 +19,76 @@ var bear = {
     noisy: true,
     deadly: true
 };
+
+var counties = [
+    "Belknap",
+    "Strafford",
+    "Carolll",
+    "Rick"
+];
+
+var cities = [
+    "Toronto",
+    "Missisauga",
+    "New York",
+    "Orlando",
+    "Sao Paulo"
+];
+
+
+
+var string1 = "This is a small string.";
+var string2 = "This is a greater string.";
+var string3 = "Is this the thing called mont Everes?";
+var string4 = "This is the Sherman on the Mont.";
+
+var regex = /this/;
+
+console.log("------START /this/--------");
+console.log("String1: " + regex.test(string1));
+console.log("String2: " + regex.test(string2));
+console.log("String3: " + regex.test(string3));
+console.log("String4: " + regex.test(string4));
+console.log("------end--------");
+
+regex = /this/i;
+console.log("------START /this/i;--------");
+console.log("String1: " + regex.test(string1));
+console.log("String2: " + regex.test(string2));
+console.log("String3: " + regex.test(string3));
+console.log("String4: " + regex.test(string4));
+
+regex = /^this/i;
+console.log("------START /^this/i;-------");
+console.log("String1: " + regex.test(string1));
+console.log("String2: " + regex.test(string2));
+console.log("String3: " + regex.test(string3));
+console.log("String4: " + regex.test(string4));
+
+regex = /ever/i;
+console.log("------START /ever/i;--------");
+console.log("String1: " + regex.test(string1));
+console.log("String2: " + regex.test(string2));
+console.log("String3: " + regex.test(string3));
+console.log("String4: " + regex.test(string4));
+
+regex = /ever.$/i;
+console.log("------START /ever.$/i;--------");
+console.log("String1: " + regex.test(string1));
+console.log("String2: " + regex.test(string2));
+console.log("String3: " + regex.test(string3));
+console.log("String4: " + regex.test(string4));
+
+regex = /ever\.$/i;
+console.log("------START /ever\.$/i;--------");
+console.log("String1: " + regex.test(string1));
+console.log("String2: " + regex.test(string2));
+console.log("String3: " + regex.test(string3));
+console.log("String4: " + regex.test(string4));
+
+regex = /Moun.$/i;
+console.log("------START /Moun.$/i;--------");
+console.log("String1: " + regex.test(string1));
+console.log("String2: " + regex.test(string2));
+console.log("String3: " + regex.test(string3));
+console.log("String4: " + regex.test(string4));
