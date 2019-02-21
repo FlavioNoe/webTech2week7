8f8e65a9af50bd1be8a7378e39967f3f4141621f
FlavioNoe <flavio.noe.santos@gmail.com>
2019-02-21 08:44:15 -0500

    08h43min commit

--- C0740832_Flavio_week7/js/main.js
+++ C0740832_Flavio_week7/js/main.js
@@ -1,1 +1,21 @@
-﻿// Seu código é inserido aqui!
+﻿var bird = {
+    genus: "corvus",
+    species: "corvus",
+    commonName: "raven",
+    callType: "aquawky",
+    quote: "Nevermore",
+    maxOffspring: 5,
+    noisy: true,
+    deadly: false
+};
+
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: true,
+    deadly: true
+};
